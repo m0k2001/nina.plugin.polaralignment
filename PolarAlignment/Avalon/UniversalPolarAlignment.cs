8b14f8ec288b73a06e3fd316389adf51bac46ff1
m0k2001 <m0k2001@gmail.com>
2026-04-09 23:25:21 +0200

    Restored original Avalon files - reduced connexion delay

--- PolarAlignment/Avalon/UniversalPolarAlignment.cs
+++ PolarAlignment/Avalon/UniversalPolarAlignment.cs
@@ -1,4 +1,4 @@
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace NINA.Plugins.PolarAlignment.Avalon {
     public partial class UniversalPolarAlignment : UniversalPolarAlignmentBase {
@@ -9,15 +9,6 @@ namespace NINA.Plugins.PolarAlignment.Avalon {
 
         public override float XGearRatio { get => xGearRatio; set => xGearRatio = value; }
         public override float YGearRatio { get => yGearRatio; set => yGearRatio = value; }
-
-        /// <summary>Serial: auto-scan des ports COM.</summary>
-        public UniversalPolarAlignment() : base() { }
-
-        /// <summary>TCP manuel: connexion directe host:port.</summary>
-        public UniversalPolarAlignment(string tcpHost, int tcpPort) : base(tcpHost, tcpPort) { }
-
-        /// <summary>TCP auto-scan: scan parallèle de tous les subnets actifs.</summary>
-        public UniversalPolarAlignment(int tcpPort) : base(tcpPort) { }
 
         protected override Regex GetStatusRegex() => StatusRegex();
 
--- PolarAlignment/Avalon/UniversalPolarAlignmentVM.cs
+++ PolarAlignment/Avalon/UniversalPolarAlignmentVM.cs
@@ -1,4 +1,4 @@
-using NINA.Core.Utility;
+﻿using NINA.Core.Utility;
 using NINA.Profile.Interfaces;
 using NINA.Plugins.PolarAlignment.Avalon;
 
@@ -10,98 +10,91 @@ namespace NINA.Plugins.PolarAlignment.Avalon {
 
         protected override IPolarAlignmentSystem CreateSystem() => new UniversalPolarAlignment();
 
-        protected override IPolarAlignmentSystem CreateSystemTcp(string host, int port) =>
-            new UniversalPolarAlignment(host, port);
-
-        protected override IPolarAlignmentSystem CreateSystemAutoTcp(int port) =>
-            new UniversalPolarAlignment(port);
-
-        // --- Connection mode (Avalon garde Serial par défaut, TCP disponible) ---
-
-        public override ConnectionMode ConnectionMode {
-            get => Properties.Settings.Default.AvalonConnectionMode;
-            set {
-                Properties.Settings.Default.AvalonConnectionMode = value;
-                CoreUtil.SaveSettings(Properties.Settings.Default);
-                RaisePropertyChanged();
-            }
-        }
-
-        public override string TcpHost {
-            get => Properties.Settings.Default.AvalonTcpHost;
-            set {
-                Properties.Settings.Default.AvalonTcpHost = value;
-                CoreUtil.SaveSettings(Properties.Settings.Default);
-                RaisePropertyChanged();
-            }
-        }
-
-        public override int TcpPort {
-            get => Properties.Settings.Default.AvalonTcpPort;
-            set {
-                Properties.Settings.Default.AvalonTcpPort = value;
-                CoreUtil.SaveSettings(Properties.Settings.Default);
-                RaisePropertyChanged();
-            }
-        }
-
-        // --- Paramètres moteurs ---
-
         public override bool DoAutomatedAdjustments {
             get => Properties.Settings.Default.DoAutomatedAdjustments;
-            set { Properties.Settings.Default.DoAutomatedAdjustments = value; CoreUtil.SaveSettings(Properties.Settings.Default); RaisePropertyChanged(); }
+            set {
+                Properties.Settings.Default.DoAutomatedAdjustments = value;
+                CoreUtil.SaveSettings(Properties.Settings.Default);
+                RaisePropertyChanged();
+            }
         }
 
         public override double AutomatedAdjustmentSettleTime {
             get => Properties.Settings.Default.AutomatedAdjustmentSettleTime;
-            set { Properties.Settings.Default.AutomatedAdjustmentSettleTime = value; CoreUtil.SaveSettings(Properties.Settings.Default); RaisePropertyChanged(); }
+            set {
+                Properties.Settings.Default.AutomatedAdjustmentSettleTime = value;
+                CoreUtil.SaveSettings(Properties.Settings.Default);
+                RaisePropertyChanged();
+            }
         }
 
         public override float XGearRatio {
             get => Properties.Settings.Default.AvalonXGearRatio;
             set {
-                if (value < 1) value = 1;
+                if (value < 1) { value = 1; }
                 Properties.Settings.Default.AvalonXGearRatio = value;
-                if (upa != null) upa.XGearRatio = value;
+                if (upa != null) { upa.XGearRatio = value; }
                 CoreUtil.SaveSettings(Properties.Settings.Default);
-                RaisePropertyChanged(); RaisePropertyChanged(nameof(PositionX));
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(PositionX));
             }
         }
 
         public override int XSpeed {
             get => Properties.Settings.Default.AvalonXSpeed;
-            set { Properties.Settings.Default.AvalonXSpeed = value; CoreUtil.SaveSettings(Properties.Settings.Default); RaisePropertyChanged(); }
+            set {
+                Properties.Settings.Default.AvalonXSpeed = value;
+                CoreUtil.SaveSettings(Properties.Settings.Default);
+                RaisePropertyChanged();
+            }
         }
 
         public override float YGearRatio {
             get => Properties.Settings.Default.AvalonYGearRatio;
             set {
-                if (value < 1) value = 1;
+                if (value < 1) { value = 1; }
                 Properties.Settings.Default.AvalonYGearRatio = value;
-                if (upa != null) upa.YGearRatio = value;
+                if (upa != null) { upa.YGearRatio = value; }
                 CoreUtil.SaveSettings(Properties.Settings.Default);
-                RaisePropertyChanged(); RaisePropertyChanged(nameof(PositionY));
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(PositionY));
             }
         }
 
         public override int YSpeed {
             get => Properties.Settings.Default.AvalonYSpeed;
-            set { Properties.Settings.Default.AvalonYSpeed = value; CoreUtil.SaveSettings(Properties.Settings.Default); RaisePropertyChanged(); }
+            set {
+                Properties.Settings.Default.AvalonYSpeed = value;
+                CoreUtil.SaveSettings(Properties.Settings.Default);
+                RaisePropertyChanged();
+            }
         }
 
         public override bool ReverseAzimuth {
             get => Properties.Settings.Default.AvalonReverseAzimuth;
-            set { Properties.Settings.Default.AvalonReverseAzimuth = value; CoreUtil.SaveSettings(Properties.Settings.Default); RaisePropertyChanged(); }
+            set {
+                Properties.Settings.Default.AvalonReverseAzimuth = value;
+                CoreUtil.SaveSettings(Properties.Settings.Default);
+                RaisePropertyChanged();
+            }
         }
 
         public override bool ReverseAltitude {
             get => Properties.Settings.Default.AvalonReverseAltitude;
-            set { Properties.Settings.Default.AvalonReverseAltitude = value; CoreUtil.SaveSettings(Properties.Settings.Default); RaisePropertyChanged(); }
+            set {
+                Properties.Settings.Default.AvalonReverseAltitude = value;
+                CoreUtil.SaveSettings(Properties.Settings.Default);
+                RaisePropertyChanged();
+            }
         }
 
         public override float XBacklashCompensation {
             get => Properties.Settings.Default.AvalonXBacklashCompensation;
-            set { Properties.Settings.Default.AvalonXBacklashCompensation = value; CoreUtil.SaveSettings(Properties.Settings.Default); RaisePropertyChanged(); }
+            set {
+                Properties.Settings.Default.AvalonXBacklashCompensation = value;
+                CoreUtil.SaveSettings(Properties.Settings.Default);
+                RaisePropertyChanged();
+            }
         }
     }
 }
